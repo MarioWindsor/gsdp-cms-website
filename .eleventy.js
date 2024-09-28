9b756492e47ac5aa0877e2be47c5169c441a1f12
Aryan Sapiens <166945698+aryansrivastav1@users.noreply.github.com>
2024-09-28 19:06:33 +0530

    Change in config.yml

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("admin"); // Copy `admin/` to `_site/admin`
 	eleventyConfig.addPassthroughCopy("posts"); // Copy `posts/` to `_site/posts`
 	eleventyConfig.addPassthroughCopy("css"); // Copy `css/` to `_site/css`
 	eleventyConfig.addPassthroughCopy("media"); // Copy `media/` to `_site/media`
